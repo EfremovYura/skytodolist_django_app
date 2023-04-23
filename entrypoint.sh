3a1b0661afff434f42260355cb360c9fce1003d2
Yura <temp.yura@gmail.com>
2023-04-23 19:52:05 +0300

    change python to python3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-python manage.py migrate --check
+python3 manage.py migrate --check
 
 if [[ $? == 0 ]]; then
-    python manage.py migrate
+    python3 manage.py migrate
 fi
 
-python .\manage.py collectstatic -c --no-input
+python3 .\manage.py collectstatic -c --no-input
 exec "$@"
